6b53633380e10ae2f73b1bbbae0332b4d6fada46
Sourav Garodia <96367006+garodiaa@users.noreply.github.com>
2025-03-01 23:33:59 +0600

    BOTH COUNT UPDATE

--- scripts/main.js
+++ scripts/main.js
@@ -19,16 +19,31 @@ document.getElementById('weekday').innerText = weekday;
 document.getElementById('date').innerText = `${month} ${day} ${year}`;
 
 
+
+// getting element of task assigned
+const tasksAssigned = document.getElementById('tasks-assigned');
+// getting element of total completed task
+const totalCompleted = document.getElementById('total-completed');
+
 // alert after clicking completed button
-const btnCompleted = document.getElementById('btn-completed');
+const btnCompleted = document.querySelectorAll('.btn-completed');
+// console.log(btnCompleted);
+for(let i = 0; i < btnCompleted.length; i++) {
+    
+    btnCompleted[i].addEventListener('click', function() {
+        if (confirm('Board Updated Successfully')) {
+            btnCompleted[i].disabled = true;
+            btnCompleted[i].classList.remove('text-white','font-light');
+            btnCompleted[i].classList.add('opacity-50', 'cursor-not-allowed', 'text-gray-800','font-bold');
+            
+            // proti click a task assigned count fetch hobe and then kombe
+            const tasksAssignedCount = parseInt(tasksAssigned.innerText);
+            tasksAssigned.innerText = `0${tasksAssignedCount - 1}`;
 
-btnCompleted.addEventListener('click', function() {
-    if (confirm('Board Updated Successfully')) {
-        btnCompleted.disabled = true;
-        btnCompleted.classList.remove('text-white','font-light');
-        btnCompleted.classList.add('opacity-50', 'cursor-not-allowed', 'text-gray-800','font-bold');
-    }
-});
+            const totalCompletedCount = parseInt(totalCompleted.innerText);
+            totalCompleted.innerText = totalCompletedCount + 1;
+        }
+    });
+}
 
-// get number of task assigned
-const tasksAssigned = 
+
